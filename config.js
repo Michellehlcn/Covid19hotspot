5b83939d1fe2e17d1493411fea3670a4a1aee2bf
Michelle Hlcn <83108919+Michellehlcn@users.noreply.github.com>
2021-07-06 14:14:41 +1000

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ const config = {
   CSV: "https://docs.google.com/spreadsheets/d/1X1R3EQTR2JdP7kxGO0W0AvHf6fOzn3ZP-TN9HWsPaoc/gviz/tq?tqx=out:csv&sheet=Sheet1",
   center: [106.6957867, 10.78016587],
   zoom: 10,
-  title: "Replace with your title",
+  title: "CÁC ĐIỂM PHONG TỎA TẠI TP.HỒ CHÍ MINH",
   description:
-    "CÁC ĐIỂM PHONG TỎA TẠI TP.HỒ CHÍ MINH You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
+    "Những người mắc bệnh COVID-19 đã có một loạt các triệu chứng được báo cáo - từ các triệu chứng nhẹ đến bệnh nghiêm trọng. Các triệu chứng có thể xuất hiện 2-14 ngày sau khi phơi nhiễm với vi-rút. Bất cứ ai cũng có thể có các triệu chứng từ nhẹ đến nghiêm trọng. Gọi HOTLINE COVID-19 08 6957 7133 hoặc cho cơ sở cấp cứu địa phương. Danh sach cập nhật ngày 05/7/2021 (hcdc.vn)",
   sideBarInfo: ["Location_Name", "Address", "Phone"],
   popupInfo: ["Location_Name"],
   filters: [
